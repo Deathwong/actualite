5f32d542fceaaa52b035db255ec77e1d75e6614c
Deathwong <baptistejean457@gmail.com>
2023-02-01 21:55:35 +0100

    configuration

--- src/main/java/com/jeff/actualite/configuration/AppConfiguration.java
+++ src/main/java/com/jeff/actualite/configuration/AppConfiguration.java
@@ -1,4 +1,11 @@
 package com.jeff.actualite.configuration;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.jeff.actualite.*")
 public class AppConfiguration {
 }
