66621bd5dd80cf4cd039a48dfab6208bfcf38451
Deathwong <baptistejean457@gmail.com>
2023-02-01 20:52:14 +0100

    init commit

---
+++ src/test/java/com/jeff/actualite/ActualiteApplicationTests.java
@@ -0,0 +1,8 @@
+package com.jeff.actualite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActualiteApplicationTests {
+}
